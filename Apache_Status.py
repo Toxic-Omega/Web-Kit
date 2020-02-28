cd5260ee0909d4929036d4bcc1208c4882a38a84
Toxic-Omega <58302540+Toxic-Omega@users.noreply.github.com>
2020-02-28 13:44:58 +0100

    Add files via upload

--- Apache_Status.py
+++ Apache_Status.py
@@ -2,9 +2,11 @@ import os
 import time
 
 def Menu():
-    os.system("clear")
     os.system("sudo service apache2 start")
+    os.system("clear")
     os.system("sudo service apache2 status")
+    print("")
+    os.system("If Apache Is Not Running Then Start It!")
 loop = True
 while loop:
     Menu()
